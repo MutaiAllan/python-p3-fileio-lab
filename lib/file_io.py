2b5785653b631a7f84d506d95a5aabe5f9167a32
MutaiAllan <MutaiAllan>
2023-08-28 14:38:56 +0300

    Added file i/o methods

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name='/home/mutai/Development/code/phase3/week3/python-p3-fileio-lab/lib/test_file.txt', file_content='Added this text'):
+    with open(file_name, mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name='/home/mutai/Development/code/phase3/week3/python-p3-fileio-lab/lib/test_file.txt', append_content='Added this text'):
+    with open(file_name, mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name='/home/mutai/Development/code/phase3/week3/python-p3-fileio-lab/lib/test_file.txt'):
+    with open(file_name, mode='r', encoding='utf-8') as log_file:
+        log_file.read()
